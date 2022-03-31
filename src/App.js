f63297737790b54bb1bd265e840b86d540ac20d8
Marie-Lene Armingeon <marielene.a@gmail.com>
2022-03-31 12:31:35 +0200

    redirect file to fix routing problem

--- src/App.js
+++ src/App.js
@@ -11,47 +11,47 @@ import { createBowl, getIngredientsFromBowl } from "./library";
 const ingredients = getIngredientsFromBowl();
 
 const DEFAULT_BOWL = {
-  base: 0,
-  greens: 0,
-  veggies: 0,
-  extra: 0,
-  topping: 0,
-  dressing: 0
+    base: 0,
+    greens: 0,
+    veggies: 0,
+    extra: 0,
+    topping: 0,
+    dressing: 0,
 };
 
 export default function App() {
-  const [isVisibleModal, setIsVisibleModal] = useState(false);
-  const [bowl, setBowl] = useState(DEFAULT_BOWL);
-  function onCreateBowl(isFixedBowl) {
-    const currBowl = createBowl(isFixedBowl);
-    console.log("recipe in App created", currBowl);
-    setBowl(currBowl);
-  }
-  function closeModal() {
-    setIsVisibleModal(false);
-  }
-  useEffect(() => {
-    setTimeout(() => {
-      setIsVisibleModal(true);
-    }, 1000);
-  }, []);
-  return (
-    // <div className={loaded ? "app loaded" : "app"}>
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact>
-            <Bowl createBowl={onCreateBowl} currBowl={bowl} />
-            {/* {isVisibleModal && <Modal toggleModal={closeModal} />} */}
-          </Route>
-          <Route path="/recipe">
-            <Recipe />
-          </Route>
-          <Route path="/map">
-            <ImageMapBowl ingredients={ingredients} />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+    // const [isVisibleModal, setIsVisibleModal] = useState(false);
+    const [bowl, setBowl] = useState(DEFAULT_BOWL);
+    function onCreateBowl(isFixedBowl) {
+        const currBowl = createBowl(isFixedBowl);
+        console.log("recipe in App created", currBowl);
+        setBowl(currBowl);
+    }
+    // function closeModal() {
+    //   setIsVisibleModal(false);
+    // }
+    useEffect(() => {
+        setTimeout(() => {
+            setIsVisibleModal(true);
+        }, 1000);
+    }, []);
+    return (
+        // <div className={loaded ? "app loaded" : "app"}>
+        <div className="App">
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact>
+                        <Bowl createBowl={onCreateBowl} currBowl={bowl} />
+                        {/* {isVisibleModal && <Modal toggleModal={closeModal} />} */}
+                    </Route>
+                    <Route path="/recipe">
+                        <Recipe />
+                    </Route>
+                    <Route path="/map">
+                        <ImageMapBowl ingredients={ingredients} />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
